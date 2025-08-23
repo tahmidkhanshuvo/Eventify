d406e837b3b67c7869d4a551f8dafee2f2a1e712
asifuzzamanshanto <asifuzzaman2105@gmail.com>
2025-08-24 02:20:00 +0600

    route push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './login/login.jsx'
 import SignUpPage from './signup/signup.jsx'
+import Home from './navbar/Home.jsx'
+import AboutUs from './navbar/AboutUs.jsx'
+import Club from './navbar/Club.jsx'
+import Events from './navbar/Events.jsx'
+
+
 function App() {
-  const [count, setCount] = useState(0)
-// <LoginPage />
   return (
-    <div className="w-full h-full">
-      <SignUpPage/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/clubs" element={<Club />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
