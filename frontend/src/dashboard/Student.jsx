79bfcb7b23d2fa507206f99604cb0d1a77d6e077
Tahmid Khan <tahmid.khan.shuvo@gmail.com>
2025-08-25 17:16:47 +0600

    fix - 2

--- frontend/src/dashboard/Student.jsx
+++ frontend/src/dashboard/Student.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import SidebarDemo from "@/components/SidebarDemo";
+import SidebarDemo from "./components/SidebarDemo.jsx";
 import {
   IconArrowLeft,
   IconBrandTabler,
